3f1488e925e1809baf5c8173bdd4b838c5010d53
Sthwalo <sthwaloe@gmail.com>
2022-06-16 14:21:53 +0200

    objects

---
+++ src/Main/Main.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class Main {
+	int age = 33;
+	String firstNam = "Immaculate";
+	String lastNam = "Nyoni";
+	
+	
+	public static void main(String[] args) {
+	
+		Main myObj = new Main();
+		
+		System.out.println("Name: "+ myObj.firstNam + " " + myObj.lastNam);
+		System.out.println("Age: " + myObj.age);
+	}
+
+}
